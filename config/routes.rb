82265d6192142feb0d60a8096f981760b2897c65
Davide Tello <tello.davide@gmail.com>
2021-02-19 12:55:22 +0100

    Add ingreient now in the show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
   resources :cocktails, only: %i[show new create] do
-    resources :doses, only: %w[new create]
+    resources :doses, only: %w[create]
   end
   delete 'doses/:id', to: 'doses#destroy', as: :delete
 end
